88eb5107e861c98680b0be255594e77049f82b31
Sandy Sultan <sandysultan@gmail.com>
2021-02-04 13:32:13 +0700

    add bot reply and luxon packages for datetime wrapper

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+const { DateTime } = require("luxon");
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -69,10 +71,45 @@ var io = require('socket.io')(server);
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.emit('chat message', 'Hi, what is your first name?');
+    
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
+
+        if (!socket.hasOwnProperty('firstName')) {
+            socket.firstName = msg;
+            socket.emit('chat message', 'hi ' + msg + ', when is your birthday? YYYY-MM-DD');
+        } else {
+            if (!socket.hasOwnProperty('birthDay')) {
+                var birthDay = DateTime.fromFormat(msg, 'yyyy-MM-dd');
+                if (birthDay.isValid) {
+                    socket.birthDay = birthDay;
+                    socket.emit('chat message', 'Do you want to know how many days till your next birthday?');
+                } else {
+                    socket.emit('chat message', 'Invalid date, retype your birthday again with YYYY-MM-DD format');
+                }
+            } else {
+                var yes = ["yes", "yeah", "yup", "sure", "ya"];
+                var no = ["no", "nope", "nah"];
+                if (yes.some(e1 => msg.includes(e1))) {
+                    var nextBirthDay = socket.birthDay;
+                    var now = DateTime.local();
+                    var yearNow = now.year;
+                    nextBirthDay = nextBirthDay.set({ year: yearNow });
+                    var diff = nextBirthDay.diffNow('days').toObject().days;
+                    if (diff < 0) { //if birthday date in this year already passed
+                        nextBirthDay = nextBirthDay.set({ year: yearNow + 1 });
+                        diff = nextBirthDay.diffNow('days').toObject().days;
+                    }
+                    socket.emit('chat message', 'There are ' + Math.ceil(diff) + ' days left until your next birthday');
+                } else if (no.some(e1 => msg.includes(e1))) {
+                    socket.emit('chat message', 'Goodbye');
+                } else {
+                    socket.emit('chat message', 'uhh... is that a yes?');
+                }
+            }
+        }
     });
 });
